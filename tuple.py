8baec51c537c5d6e20f80a29dedba45b6a4b8117
shahbajkhan2023 <rk2403476@gmail.com>
2024-07-11 17:48:43 +0530

    tuple_prac

---
+++ tuple.py
@@ -0,0 +1,38 @@
+# t = (10, 20, 30, 40, 50, 60)
+# print(t[0])
+# print(t[-1])
+# print(t[100])
+
+
+# t1 = (10, 20, 30)
+# t2 = (40, 50, 60)
+# t3 = t1 + t2
+
+# print(t3)
+
+
+# t = (10, 20, 30, 40)
+# print(t)
+# print(len(t))
+# print(t.count(10))
+# print(t.index(30))
+
+
+# t = (40, 10, 30, 20)
+# t1 = sorted(t)
+# print(t1)
+# print(t)
+
+
+
+# a = 10
+# b = 20
+# c = 30
+# d = 40
+# t = a, b, c, d
+# print(t)
+
+
+# t = (x**2 for x in range(1, 6))
+# print(type(t))
+
