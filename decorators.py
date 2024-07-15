b7d84de0f25bacd4f14d190defaa3f2e25b1a39f
shahbajkhan2023 <rk2403476@gmail.com>
2024-07-15 18:50:32 +0530

    decorator

---
+++ decorators.py
@@ -0,0 +1,34 @@
+# def decor(func):
+#     def inner(name):
+#         if name == "Sunny":
+#             print('hello sunny Bad morning')
+#         else:
+#             func(name)
+#     return inner
+
+
+# @decor
+# def wish(name):
+#     print('Hello', name, "Good Morning")
+
+
+# wish('trythg')
+
+
+# def decor(func):
+#     list = ['CM', "PM", "IAS"]
+#     def inner(name):
+#         if name in list:
+#             print(f"Welcome {name}")
+#             print('We are thankful of you that you came here')
+#         else:
+#             func(name)
+#     return inner
+
+# @decor
+# def my_name(name):
+#     print(f"hello {name} Good morning")
+
+
+# name = input('Enter some name: ')
+# my_name(name)as
