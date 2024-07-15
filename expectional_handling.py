6ae129a78e86c0092d0463225e27f66882ff52bc
shahbajkhan2023 <rk2403476@gmail.com>
2024-07-15 14:59:25 +0530

    exception_handling

---
+++ expectional_handling.py
@@ -0,0 +1,32 @@
+# print('stmt-1')
+# try:
+#     print(10/0)
+# except ZeroDivisionError:
+#     print(10/2)
+# print('stmt-3')
+
+
+
+
+
+# try:
+#     print('try')
+#     print(10/0)
+# except ZeroDivisionError:
+#     print('expect')
+# finally:
+#     print('finally')
+
+
+
+
+
+try:
+    print('this is try block')
+except ZeroDivisionError:
+    print('this is a except block')
+else:
+    print('this is a else block')
+finally:
+    print('this is a finally block ')
+
