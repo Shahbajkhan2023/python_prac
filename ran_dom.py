1b113ddae01b702f2dd765b8c5809e23e0a734b3
shahbajkhan2023 <rk2403476@gmail.com>
2024-07-15 13:49:51 +0530

    random module

---
+++ ran_dom.py
@@ -0,0 +1,33 @@
+# from random import random
+
+
+# for i in range(10):
+#     print(random())
+
+
+
+# from random import *
+
+# for i in range(2):
+#     print(randint(1, 1000))
+
+
+
+# from random import *
+
+# for i in range(2):
+#     print(uniform(1, 10))
+
+
+# from random import *
+
+# for i in range(2):
+#     print(randrange(1, 10))
+
+
+
+# from random import *
+
+# list = ['shahbaj', 'khan', 'Akhtar']
+# for i in range(2):
+#     print(choice(list))
